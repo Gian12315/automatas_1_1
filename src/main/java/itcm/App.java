a6c6a939675cb57b79f6ad28c92c9b5116ad0702
Gian12315 <giancarlo.cytro@gmail.com>
2023-05-12 19:42:32 -0600

    Trying to test

--- src/main/java/itcm/App.java
+++ src/main/java/itcm/App.java
@@ -1,27 +1,43 @@
 package itcm;
 
+import java.io.FileInputStream;
+
 /**
  * Hello world!
  *
  */
 
 import java.io.FileReader;
+import java_cup.runtime.Symbol;
 
 public class App {
-    public static void main(String[] args) {
-        String input = new String("entrada.txt");
-        TablaSimbolos tbl = new TablaSimbolos();
-        Simbolo sim = new Simbolo(TipoDato.DOUBLE, "two", "2.2");
-        tbl.añadirSimbolo(sim);
+        public static void main(String[] args) {
+                String input = new String("entrada.txt");
+                TablaSimbolos tbl = new TablaSimbolos();
+                Simbolo sim = new Simbolo(TipoDato.DOUBLE, "two", "2.2");
+                tbl.añadirSimbolo(sim);
 
-        System.out.println(tbl);
+                System.out.println(tbl);
 
-        // try {
-        // Lexico lex = new Lexico(new FileReader(input));
-        // lex.parse();
-        // } catch(Exception ex) {
-        // System.out.println("Error fatal en compilación de entrada.");
-        // System.out.println("Causa: "+ex.getCause());
-        // }
-    }
+                Sintactico pars;
+                try {
+                        pars = new Sintactico(new Lexer(new FileReader("test.txt")));
+
+                        while (true) {
+                                Symbol s = pars.parse();
+                                System.out.println(s);
+                        }
+                } catch (Exception e) {
+                        System.out.println("Error " + e.getMessage());
+                        e.printStackTrace();
+                }
+
+                // try {
+                // Lexico lex = new Lexico(new FileReader(input));
+                // lex.parse();
+                // } catch(Exception ex) {
+                // System.out.println("Error fatal en compilación de entrada.");
+                // System.out.println("Causa: "+ex.getCause());
+                // }
+        }
 }
